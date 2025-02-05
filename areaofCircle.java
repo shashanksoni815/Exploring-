86bf44b8e6387678f4fe0085ae828e0166d109ed
Shashank Soni <shashanksoni815@gmail.com>
2025-02-05 23:56:28 +0530

    area

---
+++ areaofCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class areaofCircle {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int sq = r*r;
+        float pi = 3.14f;
+        System.out.println((float)sq*pi);
+    }
+}
