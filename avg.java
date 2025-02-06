761157156b312b3d313060790774df4d0bb38cf3
Shashank Soni <shashanksoni815@gmail.com>
2025-02-06 18:23:16 +0530

    avg

---
+++ avg.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class avg {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        float avg = (A + B + C)/3f;
+        System.out.println(avg);
+    }
+}
