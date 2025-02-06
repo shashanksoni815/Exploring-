953b68c7aade6a03e80d1e6bbb8419e7c1442a3c
Shashank Soni <shashanksoni815@gmail.com>
2025-02-06 23:30:02 +0530

    arithmetic Operator

---
+++ arithmetic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        int A = 10;
+        int B = 5;
+        System.out.println("Sum : " + (A+B));
+        System.out.println("Sub : " + (A-B));
+        System.out.println("Mul : " + A*B);
+        System.out.println("Div : " + A/B);
+        System.out.println("Modulo : " + A%B);
+    }
+}
