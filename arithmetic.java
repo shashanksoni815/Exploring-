72180b6d8be679de44cfc01832db6d7e521224c8
Shashank Soni <shashanksoni815@gmail.com>
2025-02-08 14:55:40 +0530

    conditional

--- arithmetic.java
+++ arithmetic.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 public class arithmetic {
     public static void main(String[] args) {
---
+++ conditional.java
@@ -0,0 +1,12 @@
+public class conditional {
+    public static void main(String[] args) {
+        // conditional statement
+        // adult
+        int age = 5;
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("not adult");
+        }
+    }
+}
