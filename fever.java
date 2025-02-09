2429bcd4f93d25f5d3eaf4f1541b5f3b740757f6
Shashank Soni <shashanksoni815@gmail.com>
2025-02-09 20:33:25 +0530

    fever

---
+++ fever.java
@@ -0,0 +1,10 @@
+public class fever {
+    public static void main(String[] args) {
+        int temp = 13;
+        if(temp>=100){
+            System.out.println("you have fever.");
+        } else{
+            System.out.println("normal temperature");
+        }
+    }
+}
