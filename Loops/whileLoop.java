2f18bb3db7ac7b362b42435d73fb87c7e505cb47
Shashank Soni <shashanksoni815@gmail.com>
2025-02-22 23:19:23 +0530

    loops

---
+++ Loops/whileLoop.java
@@ -0,0 +1,12 @@
+public class whileLoop {
+    public static void main(String args[]){
+        int a = 2;
+        // int b = 1;
+        while (a <= 20) {
+            // System.out.println(`2 x ${b} = ${a} `);
+            System.out.println( a);
+            
+            a *= 2;
+        }
+    }
+}
