4a0326728b7fb423c22fdd746c94f3795ba47fed
Shashank Soni <shashanksoni815@gmail.com>
2025-02-22 23:47:59 +0530

    for

---
+++ Loops/forLoop.java
@@ -0,0 +1,11 @@
+package Loops;
+
+public class forLoop {
+    public static void main(String[] args) {
+        int n = 25;
+        for (int i = 0; i <= n; i++) {
+            System.out.print(i + " ");
+
+        }
+    }
+}
