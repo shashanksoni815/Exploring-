db82f8987cf41e6299f98a7fc815a0b20d41f57d
Shashank Soni <shashanksoni815@gmail.com>
2025-07-03 00:00:50 +0530

    sum

--- Loops/sum.java
+++ Loops/sum.java
@@ -3,13 +3,14 @@ package Loops;
 public class sum {
     public static void main(String[] args) {
         int n = 0;
-        int s = 8;
+        int s = 5;
+        int t = 0;
 
         while (n <= s) {
-            System.out.println(n);
+            System.out.println(t);
+            t+=n;
             n++;
-            n+=n;
         }
-        System.out.println("total sum" + n);
+        System.out.println("total sum" + t);
     }
 }
