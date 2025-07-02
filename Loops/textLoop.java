937a0cc9191055d1af82d3911d5dbde898c35dcf
Shashank Soni <shashanksoni815@gmail.com>
2025-07-02 23:33:10 +0530

    text

---
+++ Loops/textLoop.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class textLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while(count < 10) {
+            System.out.println("Hello " + count);
+            count++;
+        }
+        System.out.println("count complete");
+    }
+}
