3c362a4f9799205534540925d27a038031ebf6b5
Shashank Soni <shashanksoni815@gmail.com>
2025-07-04 01:53:33 +0530

    fact

--- Loops/prime.java
+++ Loops/prime.java
@@ -1,11 +1,12 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class prime {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        for(int i=2; i < n; i++ ) {
+        // for(int i=2; i < n; i++ ) {
+        for(int i=2; i <= Math.sqrt(n) ; i++ ) {
             if ((n%i) == 0) {
                 System.out.println("not a prime: " + n);
                 sc.close();
