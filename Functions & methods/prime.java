d0e2ad005379157149b06ca80b9d2b39d86c6785
Shashank Soni <shashanksoni815@gmail.com>
2025-07-04 23:58:49 +0530

    prime

--- Functions & methods/prime.java
+++ Functions & methods/prime.java
@@ -3,14 +3,12 @@ import java.util.Scanner;
 public class prime {
     public static boolean isPrime(int a) {
         boolean isPrime = true;
-
         for(int i = 2; i < a; i++) {
             if(a%i == 0){
                 isPrime = false;
                 return isPrime;
             }
         }
-
         return isPrime;
     }
     public static void main(String[] args) {
