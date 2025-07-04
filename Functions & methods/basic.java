6d102ea457bccab47dc33227583dec7c303a9adb
Shashank Soni <shashanksoni815@gmail.com>
2025-07-04 16:34:24 +0530

    swap

--- Functions & methods/basic.java
+++ Functions & methods/basic.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class basic {
     public static int square(int a) {
         int sqrt = a*a;
@@ -8,10 +10,23 @@ public class basic {
             return true;
         } return false;
     }
+    public static void swap(int a, int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
     public static void main(String[] args) {
-        int a = 13;
+        Scanner sc = new Scanner(System.in);
+        System.err.println("Enter value of a : ");
+        int a = sc.nextInt();
+        System.err.println("Enter value of b : ");
+        int b = sc.nextInt();
         System.out.println("Square is : " + square(a));
+         swap(a, b);
+        
         System.out.println("Is Even : " + evenOdd(a));
-
+        sc.close();
     }
 }
