b6567811db72b47f278b8a0a61aa3573404b72cf
Shashank Soni <shashanksoni815@gmail.com>
2025-07-06 23:30:21 +0530

    array

--- Arrays/basic.java
+++ Arrays/basic.java
@@ -1,5 +1,20 @@
 package Arrays;
 
 public class basic {
-    
+    public static int linearSearch(int numbers[], int key) {
+        for(int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        int numbers[] = {1, 2, 3, 4, 5,10, 4, 3, 2, 1};
+        int key = 10;
+
+        System.out.println(linearSearch(numbers, key));
+    }
 }
