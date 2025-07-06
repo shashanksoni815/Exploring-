49a164cf8bfda95b6f18fb225b99c4a9c04fcac2
Shashank Soni <shashanksoni815@gmail.com>
2025-07-06 23:46:37 +0530

    array

--- Arrays/basic.java
+++ Arrays/basic.java
@@ -7,14 +7,23 @@ public class basic {
                 return i;
             }
         }
-
         return -1;
-
+    }
+    public static int stringSearch(String menu[], String order) {
+        for(int i = 0; i < menu.length; i++) {
+            if (menu[i] == order) {
+                return i;
+            }
+        }
+        return -1;
     }
     public static void main(String[] args) {
-        int numbers[] = {1, 2, 3, 4, 5,10, 4, 3, 2, 1};
-        int key = 10;
+        // int numbers[] = {1, 2, 3, 4, 5,10, 4, 3, 2, 1};
+        // int key = 10;
+        // System.out.println(linearSearch(numbers, key));
+        String menu[] = {"dosa", "papad", "idly", "roti"};
+        String order = "roti";
+        System.out.println(stringSearch(menu, order));
 
-        System.out.println(linearSearch(numbers, key));
     }
 }
