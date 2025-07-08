8c04cb6af425ad1190c3fc589a92e734da07b89a
Shashank Soni <shashanksoni815@gmail.com>
2025-07-09 00:22:49 +0530

    max sum

--- Arrays/pairs.java
+++ Arrays/pairs.java
@@ -30,6 +30,7 @@ public class pairs {
     }
 
     public static void maxSubarraySum(int max[]) {
+        // Brute Force
         int sum = 0;
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0; i < max.length; i++) {
@@ -50,25 +51,56 @@ public class pairs {
     }
 
     public static void maxSubarraySum2(int max[]) {
+        // Prefix Sum
         int maxSum = Integer.MIN_VALUE;
         int size = max.length;
         int sum = 0;
         int prefix[] = new int[size];
 
+        // to calculate prefix array
+        prefix[0] = max[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i-1] + max[i];
+        }
+
+        // calculating maximum of array
         for (int i = 0; i < max.length; i++) {
-            for (int j = i+1; j < size; j++) {
-                prefix[0] = max[0];
-                prefix[i] = prefix[i] + max[j];
+            for (int j = i; j < size; j++) {
+                
+                sum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+
+                if (maxSum < sum) {
+                    maxSum = sum;
+                }
             }
         }
+        System.out.println(maxSum);
+    }
+
+    public static void maxSubarraySum3(int largest[] ) {
+        // Kadane's Algorithm
+        int maximum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < largest.length; i++) {
+            sum = sum + largest[i];
+            if (sum < 0) {
+                sum = 0;
+            }
+            maximum = Math.max(sum, maximum);
+        }
+        System.out.println(maximum);
+
     }
 
     public static void main(String[] args) {
         // int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
         // // pair(numbers);
         // subArray(numbers);
-        int max[] = {1, -2, 6, -1, 3};
-        maxSubarraySum(max);
-        maxSubarraySum2(max);
+        // int max[] = {1, -2, 6, -1, 3};
+        int largest[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        // maxSubarraySum(max);
+        // maxSubarraySum2(max);
+        maxSubarraySum3(largest);
     }
 }
