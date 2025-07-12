45caf92f789a1b900bcc514dca59da9bb593ba9f
Shashank Soni <shashanksoni815@gmail.com>
2025-07-12 23:22:48 +0530

    Inbuild

--- Sorting/basicSorting.java
+++ Sorting/basicSorting.java
@@ -2,10 +2,20 @@ package Sorting;
 import java.util.Arrays;
 
 public class basicSorting {
-    public static void main(String[] args) {
+
+    public static void countSort() {
+        
+    }
+
+    public static void inbuildSort(int arr[]) {
         // Inbuild Sort
-        int arr[] = {1, 4, 5, 3, 2};
         Arrays.sort(arr);
+    }
+    
+
+
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 5, 3, 2};
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
