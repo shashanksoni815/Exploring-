2715cfce0f4f397c4139cb60ced93634befbf31d
Shashank Soni <shashanksoni815@gmail.com>
2025-07-12 23:27:08 +0530

    Inbuild

--- Sorting/basicSorting.java
+++ Sorting/basicSorting.java
@@ -12,12 +12,15 @@ public class basicSorting {
         Arrays.sort(arr);
     }
     
-
-
-    public static void main(String[] args) {
-        int arr[] = {1, 4, 5, 3, 2};
+    public static void print(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
+        
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 5, 3, 2};
+        inbuildSort(arr);
     }
 }
