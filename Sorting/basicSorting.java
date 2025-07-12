63984e0183e1bd4a0c24b206c813a2ecd42548e8
Shashank Soni <shashanksoni815@gmail.com>
2025-07-12 23:34:00 +0530

    count

--- Sorting/basicSorting.java
+++ Sorting/basicSorting.java
@@ -4,7 +4,26 @@ import java.util.Arrays;
 public class basicSorting {
 
     public static void countSort() {
-        
+        int arra[] = {1, 4, 1, 3, 2, 4, 3, 7};
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arra.length; i++) {
+            largest = Math.max(largest, arra[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for (int i = 0; i < arra.length; i++) {
+            count[arra[i]]++;
+        }
+        // sorting
+        int j=0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i]>0) {
+                arra[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+
     }
 
     public static void inbuildSort(int arr[]) {
