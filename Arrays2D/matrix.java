ca3d9bf3376234fc018dc4b54ed57b5794d08468
Shashank Soni <shashanksoni815@gmail.com>
2025-07-15 23:26:27 +0530

    spiral

--- Arrays2D/matrix.java
+++ Arrays2D/matrix.java
@@ -8,18 +8,33 @@ public class matrix {
         Scanner sc = new Scanner(System.in);
 
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 matrix[i][j] = sc.nextInt();
             }
         }
 
         //output
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
+            for (int j = 0; j < matrix[0].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
+
+        int key = 5;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == key) {
+                    System.out.println("matrix value is fount at: " + "("+ i + "," + j + ")");
+                 } //else{
+                //     System.out.println("key not found");
+                // }
+            }
+        }
+        //     System.out.println("key not found");
+
+
         sc.close();
     }
 }
