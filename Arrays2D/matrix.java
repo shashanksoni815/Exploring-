c44d6d34dc763beefed51de889a6e6d5f8de3196
Shashank Soni <shashanksoni815@gmail.com>
2025-07-15 13:02:10 +0530

    matrix

---
+++ Arrays2D/matrix.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class matrix {
+
+    public static void main(String[] args) {
+        int matrix[][] = new int [3][3];
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        //output
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
