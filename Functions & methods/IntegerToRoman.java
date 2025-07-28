7ec174269b6c6a58fbb3cc1490bb15d9a8334a98
Shashank Soni <shashanksoni815@gmail.com>
2025-07-28 14:52:10 +0530

    git push

---
+++ Functions & methods/IntegerToRoman.java
@@ -0,0 +1,11 @@
+public class IntegerToRoman {
+
+    public static void integerToRoman(int a) {
+        
+    }
+
+    public static void main(String[] args) {
+        int a = 20;
+        integerToRoman(a);
+    }
+}
