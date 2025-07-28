bf1afed9e2c8a402161a837c7c8bb38b8c68c48f
Shashank Soni <shashanksoni815@gmail.com>
2025-07-28 23:24:01 +0530

    reverse

--- Functions & methods/IntegerToRoman.java
+++ Functions & methods/IntegerToRoman.java
@@ -14,8 +14,27 @@ public class IntegerToRoman {
         System.out.println(roman);
     }
 
+    public static void romanToInteger(String rom) {
+        int integer = 0;
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String romanvalue[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        
+        int i = 0;
+        while (rom.length() > 0) {
+            if (rom.startsWith(romanvalue[i])) {
+                integer += values[i];
+                rom = rom.substring(romanvalue[i].length());
+            } else {
+                i++;
+            }
+        }
+        System.out.println(integer);
+    }
+
     public static void main(String[] args) {
-        int a = 20;
-        integerToRoman(a);
+        // int a = 20;
+        // integerToRoman(a);
+        String rom = "XLVII";
+        romanToInteger(rom);
     }
 }
