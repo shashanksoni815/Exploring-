9c7e046bb97aa6c96e1aa04485daabbd5665b4af
Shashank Soni <shashanksoni815@gmail.com>
2025-07-28 15:34:40 +0530

    git push

--- Functions & methods/IntegerToRoman.java
+++ Functions & methods/IntegerToRoman.java
@@ -1,7 +1,17 @@
 public class IntegerToRoman {
 
     public static void integerToRoman(int a) {
-        
+        String roman = "";
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String romanvalue[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < values.length; i++) {
+            if (a >= values[i]) {
+                roman = roman + romanvalue[i];
+                a = a-values[i];
+            }
+        }
+        System.out.println(roman);
     }
 
     public static void main(String[] args) {
