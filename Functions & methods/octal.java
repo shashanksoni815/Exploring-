d1988f5920f6b14d44f3ca9679b7b1114bfedb0a
Shashank Soni <shashanksoni815@gmail.com>
2025-07-29 22:11:37 +0530

    octal to decimal

--- Functions & methods/octal.java
+++ Functions & methods/octal.java
@@ -12,8 +12,22 @@ public class octal {
         System.out.println(octal);
     }
 
+    public static void octalToDecimal(int oct) {
+        int dec = 0;
+        int power = 0;
+        while (oct > 0) {
+            int dig = oct % 10;
+            dec = dec + dig * (int)Math.pow(8, power);
+            oct /= 10;
+            power++;
+        } 
+        System.out.println(dec);
+    }
+
     public static void main(String[] args) {
         int num = 68;
         decimalToOctal(num);
+        int oct = 104;
+        octalToDecimal(oct);
     }
 }
