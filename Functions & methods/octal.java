fb715d5db17abcad4fe86105f85b06a2dd9f9440
Shashank Soni <shashanksoni815@gmail.com>
2025-07-29 21:36:43 +0530

    octal

---
+++ Functions & methods/octal.java
@@ -0,0 +1,11 @@
+public class octal {
+
+    public static void decimalToOctal(int num) {
+        
+    }
+
+    public static void main(String[] args) {
+        int num = 68;
+        decimalToOctal(num);
+    }
+}
