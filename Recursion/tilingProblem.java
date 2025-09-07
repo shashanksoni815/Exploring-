3230d575d465838bace5d99851a8b3510d9bcc3f
Shashank Soni <shashanksoni815@gmail.com>
2025-09-07 23:03:48 +0530

    doing

--- Recursion/tilingProblem.java
+++ Recursion/tilingProblem.java
@@ -13,8 +13,26 @@ public class tilingProblem {
         int ver = ways(n-2);
         
         return (hri + ver);
+    };
+
+    public static void duplicate(String str, int idx, StringBuilder newStr, boolean map[] ) {
+        if(idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (map[currChar-'a'] == true) {
+            duplicate(str, idx+1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            duplicate(str, idx+1, newStr.append(currChar), map);
+        }
+
     }
+
     public static void main(String[] args) {
-        System.out.println(ways(5));
+        // System.out.println(ways(5));
+        String str = "appnnacollezsrdefsdge";
+        duplicate(str, 0, new StringBuilder("") , new boolean[26]);
     }
 }
