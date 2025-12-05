e603d92fac975a37306ded8104896caf35d6e02f
Shashank Soni <shashanksoni815@gmail.com>
2025-12-05 20:42:20 +0530

    oops

--- OOPS/oops.java
+++ OOPS/oops.java
@@ -4,26 +4,34 @@ public class oops {
     public static void main(String args[]) {
         Pen p1 = new Pen(); // created a pen object p1
         p1.setcolor("Blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
         p1.setTip(5);
-        System.out.println(p1.tip);
-        p1.color = "Yellow";
-        p1.tip = 6;
-        System.out.println(p1.color);
-        System.out.println(p1.tip);
+        System.out.println(p1.getTip());
+        p1.setcolor("Yellow");
+        p1.setTip(6);
+        System.out.println(p1.getColor());
+        System.out.println(p1.getTip());
     }
 }
 
 class Pen {
-    String color;
-    int tip;
+    private String color;
+    private int tip;
 
-    void setcolor(String newColor) {
-        color = newColor;
+    String getColor() {
+        return this.color;
     }
 
-    void setTip(int newTip) {
-        tip = newTip;
+    int getTip() {
+        return this.tip;
+    }
+
+    void setcolor(String color) {
+        this.color = color;
+    }
+
+    void setTip(int tip) {
+        this.tip = tip;
     }
 }
 
